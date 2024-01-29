e6d10979105bfbd1069407ce7d89ee6e8e71d66d
Ciprian Regus <ciprian.regus@analog.com>
2024-01-29 10:45:32 +0200

    add a custom linker script for external RAM and flash

--- tools/scripts/maxim.mk
+++ tools/scripts/maxim.mk
@@ -94,7 +94,7 @@ endif
 
 INCS += $(foreach dir,$(DRIVER_INCLUDE_DIR), $(wildcard $(dir)/*.h))
 
-LSCRIPT = $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/GCC/$(TARGET_LCASE)_apard.ld
+LSCRIPT = $(PROJECT)/$(TARGET_LCASE)_apard.ld
 ASM_SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/GCC/startup_$(TARGET_LCASE).S
 SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/heap.c
 SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/system_$(TARGET_LCASE).c
