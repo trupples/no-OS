a6db754458353ad8ac5f3e948120607b39616e7b
Axel Haslam <ahaslam@baylibre.com>
2024-04-29 14:54:12 +0200

    projects: Add ad796x_fmcz
    This adds the project files for ad796x.
    It supports iio and basic example for
    the xilinx platform.
    
    Signed-off-by: Axel Haslam <ahaslam@baylibre.com>

---
+++ projects/ad796x_fmcz/src/examples/examples_src.mk
@@ -0,0 +1,20 @@
+ifeq (y,$(strip $(IIO_EXAMPLE)))
+CFLAGS += -DIIO_EXAMPLE=1
+SRCS += $(PROJECT)/src/examples/iio_example/iio_example.c
+INCS += $(PROJECT)/src/examples/iio_example/iio_example.h
+IIOD=y
+SRC_DIRS += $(NO-OS)/iio/iio_app
+endif
+
+ifeq (y,$(strip $(BASIC_EXAMPLE)))
+CFLAGS += -DBASIC_EXAMPLE=1
+SRCS += $(PROJECT)/src/examples/basic_example/basic_example.c
+INCS += $(PROJECT)/src/examples/basic_example/basic_example.h
+endif
+
+INCS += $(DRIVERS)/adc/ad796x/ad796x.h
+
+SRCS += $(DRIVERS)/adc/ad796x/ad796x.c
+
+INCS += $(INCLUDE)/no_os_list.h \
+        $(PLATFORM_DRIVERS)/$(PLATFORM)_uart.h
